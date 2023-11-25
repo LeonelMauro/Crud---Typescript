5addfbd82b93af33e497dd5297d8498f23c675ff
leonel <leoonelvj@gmail.com>
2023-11-25 20:57:45 -0300

    agregando archivo DeleteProductController.ts

---
+++ src/controllers/DeleteProductController.ts
@@ -0,0 +1,25 @@
+import {Request , Response } from "express";
+import { DeleteProductService } from "../services/DeleteProductService";
+
+class DeleteProductController {
+    async handle (request : Request , response: Response){
+        const id = request.body;
+        const deleteProductService = new DeleteProductService ();
+        
+        try {
+            await deleteProductService.delete(id).then(() => {
+              response.render("message", {
+                message: "Usuário deletado com sucesso"
+              });
+            });
+          } catch (err) {
+            response.render("message", {
+              message: `Erro ao deletar usuário: ${err.message}`
+            });
+          }
+        }
+      }
+      
+export { DeleteProductController };
+
+ 
