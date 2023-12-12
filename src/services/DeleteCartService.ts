13a9bd9b5e32bac50c44c934e670b57ae67bfbbb
leonel <leoonelvj@gmail.com>
2023-12-12 01:54:07 -0300

    iniciando eliminar producto del carro de compra, nuevo archivo DeleteCartService.ts

---
+++ src/services/DeleteCartService.ts
@@ -0,0 +1,20 @@
+import { CartRepository } from "../repositories/CartRepository";
+import {CartItem} from "../entities/CartItem"
+import {getCustomRepository} from "typeorm"
+
+class DeleteCartService{
+    async delete (id: string){
+        const cartRepository = getCustomRepository(CartRepository);
+
+        const cart = await cartRepository
+        .createQueryBuilder ()
+        .delete ()
+        .from(CartItem)
+        .where ("id : id",{id})
+        .execute ();
+
+    return cart
+    }
+}
+
+export { DeleteCartService}
